74624a24b3ef957789ef0f250c404e5e5318f7dc
OStefani <ostefanishina@gmail.com>
2018-01-01 18:16:06 +0300

    some commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require('http');
+var port = process.env.API_PORT || 3000;
+
 var server = http.createServer(function(req, res) {
 res.writeHead(200, {'Content-Type': 'application/json'});
 res.end('Hello World');
 });
 
-server.listen(8080, '188.226.185.203', function() {
-console.log("Server started")});
-
-
+server.listen(port, 'localhost', function() {
+console.log(`Server started at ${port}`)});
