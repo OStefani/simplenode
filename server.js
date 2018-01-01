d8342b3f9417a4003b6d9a5c113b0b4a31c6bb1f
OStefani <ostefanishina@gmail.com>
2018-01-01 16:52:11 +0300

    first

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ res.writeHead(200, {'Content-Type': 'application/json'});
 res.end('Hello World');
 });
 
-server.listen(8080, '188.166.97.82', function() {
+server.listen(8080, '188.226.185.203', function() {
 console.log("Server started")});
 
 
